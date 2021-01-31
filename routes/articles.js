50b6b104b8fa6e3401bb22ed4fad7b8ab479dc12
OmarMo7 <omar7.first@gmail.com>
2021-01-31 16:28:05 +0200

    Fixing a god damn bug!

--- routes/articles.js
+++ routes/articles.js
@@ -2,12 +2,15 @@ const express = require('express')
 const Article = require('./../models/article')
 const router = express.Router()
 
+
 router.get('/new', (req, res) => {
   res.render('articles/new', { article: new Article() })
 })
 
 router.get('/edit/:id', async (req, res) => {
+  // console.log(req.params.id)
   const article = await Article.findById(req.params.id)
+  // console.log(article.textType)
   res.render('articles/edit', { article: article })
 })
 
@@ -19,8 +22,12 @@ router.get('/edit/:id', async (req, res) => {
 // })
 
 router.get('/:slug', async (req, res) => {
+  
   const article = await Article.findOne({ slug: req.params.slug })
   if (article == null) res.redirect('/')
+  // console.log(article.title)
+  // console.log(article.slug)
+  // console.log(article.id)
   res.render('articles/show', { article: article })
 })
 
@@ -30,7 +37,9 @@ router.post('/', async (req, res, next) => {
 }, saveArticleAndRedirect('new'))
 
 router.put('/:id', async (req, res, next) => {
-  req.article = await Article.findById(req.params.id)
+  // console.log(req.params.id)
+  req.article = await Article.findOne({ slug: req.params.id }) //MOTHERFUCKIN LINE!!
+  // console.log(req.article)
   next()
 }, saveArticleAndRedirect('edit'))
 
@@ -45,10 +54,14 @@ function saveArticleAndRedirect(path) {
     article.title = req.body.title
     article.description = req.body.description
     article.markdown = req.body.markdown
+    article.textType = req.body.textType
+    // console.log("article before saving: " + article)
     try {
       article = await article.save()
       res.redirect(`/articles/${article.slug}`)
+      // console.log("article after saving: "+ article)
     } catch (e) {
+      // console.log(e)
       res.render(`articles/${path}`, { article: article })
     }
   }
