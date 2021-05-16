04769ed1a003e380f8b0d2abe21e3a9f637ae4df
OmarMo7 <omar7.first@gmail.com>
2021-05-16 13:52:52 +0200

    Bug fix: back button with input filled faults

--- public/js/login.js
+++ public/js/login.js
@@ -3,6 +3,9 @@ back.addEventListener('click', () => {
   var name = document.getElementById('name')
   var password = document.getElementById('password')
 
+  name.value = null
+  password.value = null
+
   name.removeAttribute('required')
   password.removeAttribute('required')
   console.log(name)
