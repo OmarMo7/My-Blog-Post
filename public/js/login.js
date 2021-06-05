abbb79d5ee15ed2bf7dd286f13377c0497dbadd2
OmarMo7 <omar7.first@gmail.com>
2021-06-05 23:48:16 +0200

    Change the logic of logging in

--- public/js/login.js
+++ public/js/login.js
@@ -1,13 +1,25 @@
 var back = document.getElementById('back')
+var login = document.getElementById('submitLogin')
+var username = document.getElementById('name')
+var password = document.getElementById('password')
+console.log(username)
+console.log(password)
+
 back.addEventListener('click', () => {
-  var name = document.getElementById('name')
-  var password = document.getElementById('password')
-
-  name.value = null
-  password.value = null
-
-  name.removeAttribute('required')
+  if (username != null && password != null) {
+    username.value = null
+    password.value = null
+  }
+  username.removeAttribute('required')
   password.removeAttribute('required')
-  console.log(name)
-  console.log(password)
 })
+
+login.addEventListener('click', () => {
+  console.log('login')
+  localStorage.setItem('username', username.value)
+  localStorage.setItem('password', password.value)
+})
+
+
+username.value = localStorage.getItem('username')
+password.value = localStorage.getItem('password')
